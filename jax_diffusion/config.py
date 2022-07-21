32735449236bf9818ee65fc7c86fa5537281911a
andylolu2 <andylolu24@gmail.com>
2022-07-21 23:26:44 +0100

    Add dropout and try on new datasets

--- jax_diffusion/config.py
+++ jax_diffusion/config.py
@@ -8,42 +8,46 @@ def get_config() -> ConfigDict:
 
     config.project_name = "jax-diffusion"
 
-    # config.restore = str(Path.cwd() / "checkpoints/whole-sunset-81")
+    # config.restore = str(Path.cwd() / "checkpoints/deft-voice-105")
     config.restore = None
 
     d_model = 32
-    training_steps = 20000
+    training_steps = 30000
 
     config.seed = 42
     config.training_steps = training_steps
     config.ckpt_dir = str(Path.cwd() / "checkpoints")
     config.log_interval = 1
-    config.ckpt_interval = 30
-    config.eval_interval = 60
+    config.ckpt_interval = 60
+    config.eval_interval = 180
 
     config.experiment_kwargs = ConfigDict(
         dict(
             config=dict(
                 seed=42,
                 dataset=dict(
-                    resize_dim=32,
+                    name="celeb_a",
+                    resize_dim=64,
                     data_dir=str(Path.home() / "tensorflow_datasets"),
                 ),
                 training=dict(
                     batch_size=64,
                     subset="100%",
+                    ema_step_size=1 - 0.9995,
                     optimizer=dict(
                         optimizer_type="adam",
-                        kwargs=dict(),
+                        kwargs=dict(
+                            max_grad_norm=1.0,
+                        ),
                         lr_schedule=dict(
                             # schedule_type="constant",
                             # kwargs=dict(
-                            #     value=3e-4,
+                            #     value=3e-6,
                             # ),
                             schedule_type="cosine",
                             kwargs=dict(
                                 init_value=0,
-                                peak_value=3e-4,
+                                peak_value=4e-4,
                                 warmup_steps=500,
                                 decay_steps=training_steps,
                                 decay_factor=0.1,
@@ -65,7 +69,7 @@ def get_config() -> ConfigDict:
                     unet_kwargs=dict(
                         dim_init=d_model,
                         kernel_size_init=3,
-                        dim_mults=(1, 2, 2, 2),
+                        dim_mults=(1, 1, 2, 2, 2),
                         attention_resolutions=(16,),
                         attention_num_heads=4,
                         num_res_blocks=2,
@@ -73,6 +77,7 @@ def get_config() -> ConfigDict:
                         time_embed_dim=4 * d_model,
                         kernel_size=3,
                         num_groups=4,
+                        dropout=0.1,
                     ),
                 ),
             )
