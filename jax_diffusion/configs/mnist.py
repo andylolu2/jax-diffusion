ba10d70c6b8a590e74ce5163c2a69d33845fcf33
andylolu2 <andylolu24@gmail.com>
2022-08-11 00:33:41 +0100

    Change mnist train config

--- jax_diffusion/configs/mnist.py
+++ jax_diffusion/configs/mnist.py
@@ -5,13 +5,16 @@ from jax_diffusion.types import Config
 def get_config() -> Config:
     config = get_base_config()
 
-    config.effective_steps = 40000
+    config.effective_steps = 50000
+    config.ckpt_interval = 60
+    config.eval_interval = 60
 
     exp_conf = config.experiment_kwargs.config
 
     exp_conf.dataset_kwargs.name = "mnist"
     exp_conf.dataset_kwargs.resize_dim = 32
-    exp_conf.train.dataset_kwargs.batch_size = 64
+    exp_conf.train.dataset_kwargs.batch_size = 128
+
     exp_conf.model.unet_kwargs.dim_init = 32
     exp_conf.model.unet_kwargs.dim_mults = (1, 2, 2, 2)
 
