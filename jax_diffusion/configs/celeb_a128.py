97daa89f8ef290083ec6847bbdbd011ae9c6b261
andylolu2 <andylolu24@gmail.com>
2022-08-21 01:27:47 +0100

    Update celeb128 config

--- jax_diffusion/configs/celeb_a128.py
+++ jax_diffusion/configs/celeb_a128.py
@@ -12,14 +12,14 @@ def get_config() -> Config:
     exp_conf = config.experiment_kwargs.config
     exp_conf.dataset_kwargs.name = "celeb_a"
     exp_conf.dataset_kwargs.resize_dim = 128
-    exp_conf.train.dataset_kwargs.batch_size = 32
-    exp_conf.eval.dataset_kwargs.batch_size = 32
+    exp_conf.train.dataset_kwargs.batch_size = 64
+    exp_conf.eval.dataset_kwargs.batch_size = 64
     exp_conf.eval.dataset_kwargs.subset = "20%"
     exp_conf.eval.sample_kwargs.num = 6
 
     exp_conf.train.optimizer.kwargs.grac_acc_steps = 1
 
-    exp_conf.model.unet_kwargs.dim_init = 96
+    exp_conf.model.unet_kwargs.dim_init = 128
     exp_conf.model.unet_kwargs.dim_mults = (1, 1, 2, 2, 2)
 
     return config
