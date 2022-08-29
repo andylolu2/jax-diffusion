7fb27f85681dd7fac9629f2569da1674d903d4be
andylolu2 <andylolu24@gmail.com>
2022-08-29 16:15:32 +0100

    Update celeb_a 64 config

--- jax_diffusion/configs/celeb_a64.py
+++ jax_diffusion/configs/celeb_a64.py
@@ -5,13 +5,14 @@ from jax_diffusion.types import Config
 def get_config() -> Config:
     config = get_base_config()
 
-    config.effective_steps = 40000
+    config.effective_steps = 100000
 
     exp_conf = config.experiment_kwargs.config
     exp_conf.dataset_kwargs.name = "celeb_a"
     exp_conf.dataset_kwargs.resize_dim = 64
+    exp_conf.dataset_kwargs.data_dir = "gs://jax-diffusion-dataset"
     exp_conf.train.dataset_kwargs.batch_size = 64
-    exp_conf.model.unet_kwargs.dim_init = 64
-    exp_conf.model.unet_kwargs.dim_mults = (1, 1, 2, 2, 4, 4)
+    exp_conf.model.unet_kwargs.dim_init = 128
+    exp_conf.model.unet_kwargs.dim_mults = (1, 2, 2, 4)
 
     return config
